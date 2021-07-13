409eeac7872f9a8aa5d2472ac5fe76706109b197
Harold Burbano <haroldburbano2002@gmail.com>
2021-07-13 18:19:53 -0400

    Se quito el margin a los hovers pues ya estaban definidos mas adelante

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
+const cuadro = document.getElementsByClassName("overscroll__box");
 
-  let onCuadro = false;
+for (let i = 0; i < cuadro.length; i++) {
+  cuadro[i].addEventListener("mouseenter", verdadero);
+}
 
-  const cuadro = document.getElementsByClassName("overscroll__box");
-  const contenedorCuadro = document.getElementsByClassName(
-    "overscroll__container"
-  );
+for (let i = 0; i < cuadro.length; i++) {
+  cuadro[i].addEventListener("mouseleave", falso);
+}
 
-  for (let i = 0; i < cuadro.length; i++) {
-    cuadro[i].addEventListener("mouseenter", verdadero);
+function verdadero() {
+  let fatherId = getParentId(this);
+  let hijoClass = "";
+
+  if (fatherId === "cont-1") {
+    hijoClass = "container-1";
+  } else if (fatherId === "cont-2") {
+    hijoClass = "container-2";
+  } else {
+    hijoClass = "container-3";
   }
 
-  for (let i = 0; i < cuadro.length; i++) {
-    cuadro[i].addEventListener("mouseleave", falso);
+  let siblings = getSiblings(document.querySelector(`.${hijoClass}`));
+
+  if (screen.width < 800) {
+    this.classList.add("container-hover-movil");
+  } else {
+    this.classList.add("container-hover-movil");
   }
 
-  function verdadero() {
-    onCuadro = true;
-  }
-
-  function falso() {
-    console.log("onCuadro = false");
-    onCuadro = false;
-  }
-
-  for (let i = 0; i < contenedorCuadro.length; i++) {
-    contenedorCuadro[i].addEventListener("mouseover", accionContenedor);
-  }
-
-  function accionContenedor(e)
-  {
-    if (!onCuadro) {
-      
+  for(let i=0;i<siblings.length;i++){
+    if (screen.width < 800) {
+      siblings[i].classList.add("container-movil");
     } else {
-      console.log(this);
-      this.classList.add("hover")
-    } 
+      siblings[i].classList.add("container-movil");
+    }
   }
+}
 
+// function falso() {
+//   for (let i = 0; i < cuadro.length; i++) {
+//     cuadro[i].classList.remove(
+//       "container-hover-movil",
+//       "container-hover-computador",
+//     );
+//   }
+// }
+
+let getSiblings = function (e) {
+  // for collecting siblings
+  let siblings = [];
+  // if no parent, return no sibling
+  if (!e.parentNode) {
+    return siblings;
+  }
+  // first child of the parent node
+  let sibling = e.parentNode.firstChild;
+
+  // collecting siblings
+  while (sibling) {
+    if (sibling.nodeType === 1 && sibling !== e) {
+      siblings.push(sibling);
+    }
+    sibling = sibling.nextSibling;
+  }
+  return siblings;
+};
+
+function getParentId(hijo) {
+  return hijo.parentElement.getAttribute("id");
+}
